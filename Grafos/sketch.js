0f93b6bbb9da726ab97696e3924c6539e0d8270f
Caio Oliveira de Moraes <caiooliv97@gmail.com>
2019-07-02 13:36:13 -0300

    Criado DFS

--- Grafos/sketch.js
+++ Grafos/sketch.js
@@ -1,8 +1,13 @@
 var matrix = [];
 var grafo = [];
 const nos = 6;
+var noInicial;
+var pilha = [];
+var visitados = []
+
 
 function setup() {
+  noInicial = parseInt(random(0,5));  
   createCanvas(800,800);
   background(0)
   textSize(32)
@@ -11,20 +16,26 @@ function setup() {
   var posicoes = []
   valores.push(0)
   valores.push(1)
+  //Cria martiz de adjacencia 
   for ( let y = 0; y < nos; y++ ) {
     matrix[ y ] = [];
     for ( let x = 0; x < nos; x++ ) {
         matrix[ y ][ x ] = random(valores);
     }
   }
-
+  //Cria grafo
   for(let i = 0; i < nos; i++){
     posicoes = calcPosicao(i+1)
     grafo.push(new No(posicoes[0] ,posicoes[1],matrix[i],i))
   }
+
+  DFS(noInicial)
   
-  console.log(grafo)
+  
+  
+  //console.log(grafo)
   console.log(matrix.join('\n'))
+
 }
 
 function calcPosicao(i){
@@ -60,6 +71,19 @@ function calcPosicao(i){
 }
 
 function draw() {
+  
+  fill(255)
+  noStroke()
+  text("DFS",370,100)
+  text("Pilha",650,100)
+  
+  stroke(255)
+  fill(0)
+  rect(650,150,60,60)
+  noStroke()
+  fill(255)
+   
+  
   for(let i = 0; i < nos; i++){
     
     fill(255)
@@ -68,11 +92,34 @@ function draw() {
       if(grafo[i].vizinhos[j] === 1){
         line(grafo[i].x,grafo[i].y,grafo[j].x,grafo[j].y)
       }
+      circle(grafo[i].x,grafo[i].y,50)
     }
-    circle(grafo[i].x,grafo[i].y,50)
     fill(0)
     text(i,grafo[i].x,grafo[i].y)    
   }
+}
+
+function DFS(elemento){
+  
+  visitados.push(grafo[elemento]);  
+
+  for(let i = 0; i < nos;i++){
+    if(grafo[elemento].vizinhos[i] === 1){
+      pilha.push(grafo[i])
+      console.log("adicionado "+i+" a pilha de visitados")
+    }   
+  }
+  
+  let primeiroFila = pilha.splice(0,1);
+  if(visitados.length === 6){
+    return
+  }
+  DFS(primeiroFila[0].index)
+
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 class No{
@@ -81,6 +128,7 @@ class No{
         this.y = y;
         this.vizinhos = vizinhos;
         this.index = index;
+        this.visitado = false
     }
 }
 
